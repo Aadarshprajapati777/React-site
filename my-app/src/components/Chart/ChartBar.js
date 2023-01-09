a8992c28844504be92e90ef0752feefe82e963c2
Aadarsh <aadarsh9811594152@gmail.com>
2023-01-09 21:00:54 +0530

    month and total expenditure added in the chart bar

--- my-app/src/components/Chart/ChartBar.js
+++ my-app/src/components/Chart/ChartBar.js
@@ -12,7 +12,7 @@ const ChartBar=(props)=>{
             <div className='chart-bar__inner'>
                 <div className='chart-bar__fill' style={{height: barFillHeight}}></div>
             </div>
-            <div className='chart-bar__label'>{barFillHeight}</div>
+            <div className='chart-bar__label'>{props.label} {props.value}</div>
         </div>
         )
 }
